205e1357714c738c9963cceb23e0bc53d8e94155
chenglulu <919903021@qq.com>
2020-04-21 13:01:01 +0800

    测试自动提交

--- build.sh
+++ build.sh
@@ -12,34 +12,34 @@ echo "branch $br"
 
 
 if [ $1 == "dev" ]; then
-  if [[ $br == 'master' ]]; then
+  if [ $br == 'master' ]; then
     echo "\033[41;36m pleace checkout branch to dev \033[0m"
-  elif [[ $br == 'dev' ]]; then
+  elif [ $br == 'dev' ]; then
     echo "git autopush start..."
     git add .
     git commit -m $2
     git push origin dev
     echo "git autopush end..."
   fi
-elif [[ $1 == "online" ]]
+elif [ $1 == "online" ]
 then
-  if [[ $br == 'dev' ]]; then
+  if [ $br == 'dev' ]; then
     git add .
     git commit -m ${onlineDevCommit}
     git checkout master
     git merge dev
     echo "git auto checkout master and merge dev..."
   fi
-    npm run build
-    echo "git autopush start..."
-    git add .
-    git commit -m $2
-    # git checkout master
-    git push origin master
-    echo "git autopush end..."
-    echo "git autopush start chengllNice.github.io..."
-    npm run auto:push
-    echo "git autopush end chengllNice.github.io..."
+  npm run build
+  echo "git autopush start..."
+  git add .
+  git commit -m $2
+  # git checkout master
+  git push origin master
+  echo "git autopush end..."
+  echo "git autopush start chengllNice.github.io..."
+  npm run auto:push
+  echo "git autopush end chengllNice.github.io..."
 else
     echo "environment is invalid"
 fi
