9024aaa322875d7ff7edcc1391e00cfb5c6ff8e5
chenglulu <919903021@qq.com>
2020-04-21 12:43:37 +0800

    测试自动提交

--- build.sh
+++ build.sh
@@ -3,18 +3,34 @@
 echo "build environment：$1";
 echo "commit ：$2";
 
-testCommitMsg="build file into dist directory for test"
-onlineCommitMsg="build file into dist directory for online"
+onlineDevCommit="commit dev and checkout master"
+devMasterCommit="commit master and checkout dev"
 
-if [[ $1 == "dev" ]]; then
+branch=`git branch | grep "*"` # 获取分支名
+br=${branch:2} # 去除多余的*
+echo "branch $br"
+
+
+if [ $1 == "dev" ]; then
+  echo "branch 123 $br"
+  if [[ $br == 'master' ]]; then
+    echo -e "\033[41:36m pleace checkout branch to dev \033[0m"
+  elif [[ $br == 'dev' ]]; then
     echo "git autopush start..."
     git add .
     git commit -m $2
-    # git checkout dev
     git push origin dev
     echo "git autopush end..."
+  fi
 elif [[ $1 == "online" ]]
 then
+  if [[ $br == 'dev' ]]; then
+    git add .
+    git commit -m ${onlineDevCommit}
+    git checkout master
+    git merge dev
+    echo "git auto checkout master and merge dev..."
+  fi
     npm run build
     echo "git autopush start..."
     git add .
@@ -22,6 +38,9 @@ then
     # git checkout master
     git push origin master
     echo "git autopush end..."
+    echo "git autopush start chengllNice.github.io..."
+    npm run auto:push
+    echo "git autopush end chengllNice.github.io..."
 else
     echo "environment is invalid"
 fi
